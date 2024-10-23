d117f840ab8bbb7d93841bea427d79faa6161fe1
fatalistix <slavabal11@outlook.com>
2024-10-23 17:07:56 +0700

    added entry point

---
+++ AllOfTheHackathon/AllOfTheHackathon/Program.cs
@@ -0,0 +1,19 @@
+﻿using AllOfTheHackathon.Contracts;
+using AllOfTheHackathon.Repository;
+using AllOfTheHackathon.Service;
+using AllOfTheHackathon.Service.Transient;
+using AllOfTheHackathon.TeamBuildingStrategy;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args).ConfigureServices((_, services) =>
+{
+    services.AddHostedService<HackathonWorker>();
+    services.AddTransient<Hackathon>();
+    services.AddTransient<ITeamBuildingStrategy, GaleShapleyTeamBuildingStrategy>();
+    services.AddTransient<HrManager>();
+    services.AddTransient<HrDirector>();
+    services.AddTransient<EmployeeCsvRepository>();
+}).Build();
+
+host.Run();
