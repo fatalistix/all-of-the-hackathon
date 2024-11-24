f61c32af217056067696a6f6172270a38772f10f
fatalistix <slavabal11@outlook.com>
2024-11-24 17:27:51 +0700

    added usage of extracted method

--- AllOfTheHackathon/AllOfTheHackathon/Service/Transient/Hackathon.cs
+++ AllOfTheHackathon/AllOfTheHackathon/Service/Transient/Hackathon.cs
@@ -7,6 +7,8 @@ public class Hackathon
 {
     public (IList<Wishlist> teamLeadsWishlists, IList<Wishlist> juniorsWhishLists) Hold(in IList<Employee> teamLeads, in IList<Employee> juniors)
     {
+        Validate(teamLeads, juniors);
+        
         var teamLeadsWishlists = new List<Wishlist>();
         var juniorsWishLists = new List<Wishlist>();
         
@@ -27,7 +29,7 @@ public class Hackathon
         return (teamLeadsWishlists, juniorsWishLists);
     }
 
-    private void Validate(in IList<Employee> teamLeads, in IList<Employee> juniors)
+    private static void Validate(in IList<Employee> teamLeads, in IList<Employee> juniors)
     {
         if (teamLeads.Count != juniors.Count)
         {
