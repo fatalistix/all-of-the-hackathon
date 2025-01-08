7989511bb0509c62d62a25fde0f4fa8ef4cf258c
Viacheslav Balashov <slavabal11@outlook.com>
2025-01-08 20:47:44 +0700

    updated

--- AllOfTheHackathon/HrManagerHttpService/Program.cs
+++ AllOfTheHackathon/HrManagerHttpService/Program.cs
@@ -2,25 +2,46 @@
 using AllOfTheHackathon.Service.Transient;
 using AllOfTheHackathon.TeamBuildingStrategy;
 using HrManagerHttpService.Clients;
+using HrManagerHttpService.Databases.Contexts;
+using HrManagerHttpService.Mappers;
 using HrManagerHttpService.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Refit;
 
 var builder = WebApplication.CreateBuilder();
 builder.Services.AddControllers();
-builder.Services.AddSingleton<CollectingService>();
+builder.Services.AddHostedService<CheckingService>();
+builder.Services.AddTransient<CollectingService>();
 builder.Services.AddTransient<HrManagerService>();
 builder.Services.AddTransient<HrManager>();
 builder.Services.AddTransient<ITeamBuildingStrategy, GaleShapleyTeamBuildingStrategy>();
-builder.Services.AddTransient<ITeamsAndWishlistsSender>(_ => RestService.For<ITeamsAndWishlistsSender>("http://hr-director:6970"));
-builder.WebHost.ConfigureKestrel(options =>
+builder.Services.AddTransient<ITeamsAndWishlistsSender>(_ =>
+    RestService.For<ITeamsAndWishlistsSender>("http://hr-director:6970"));
+builder.Services.AddAutoMapper(typeof(EmployeeWithDesiredEmployeesProfile));
+builder.Services.AddHealthChecks();
+builder.Services.AddDbContext<HrManagerContext>(optionsBuilder =>
 {
-    options.ListenAnyIP(6969);
+    optionsBuilder.UseNpgsql(
+        "Host=db;" +
+        "Port=5432;" +
+        "Database=hr-manager;" +
+        "Username=all-of-the-hackathon-owner;" +
+        "Password=all-of-the-hackathon-password");
+    optionsBuilder.EnableSensitiveDataLogging();
+    optionsBuilder.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
 });
+builder.WebHost.ConfigureKestrel(options => { options.ListenAnyIP(6969); });
+builder.Configuration.AddJsonFile("appsettings.HrManager.json", true, true);
 var app = builder.Build();
 
 app.MapControllers();
+app.MapHealthChecks("/healthz");
+
+using (var serviceScope = app.Services.CreateScope())
+{
+    var context = serviceScope.ServiceProvider.GetRequiredService<HrManagerContext>();
+    context.Database.Migrate();
+}
 
 app.Run();
